c8c6e7d46a0d9833e42540cf6b7d94430751e4bc
Nils Lundquist <nils@bitovi.com>
2020-01-01 16:42:15 -0800

    add test case for issue with scrolltop

--- test/test-core.js
+++ test/test-core.js
@@ -30,4 +30,45 @@ describe('haunted/core', () => {
       teardown();
     });
   })
+
+  describe('Components', () => {
+    it('has Element prototype setters that are working as expected', async () => {
+      const tag = 'component-scrolltop-test';
+
+      const { component } = haunted({
+        render(what, where) {
+          render(what, where);
+        }
+      });
+
+      function App() {
+        function onClick() {
+          this.scrollTop = 400;
+        }
+        return html`
+          <style>
+            :host {
+              height:500px;
+              overflow: scroll;
+            }
+            .long {
+                width: 30px;
+                height: 1000px; 
+                background: blue;            
+            }
+          </style>         
+          <div class="long"></div>
+        `;
+      }
+
+      customElements.define(tag, component(App));
+
+      let teardown = attach(tag);
+      await cycle();
+
+      host.firstChild.scrollTop = 400;
+      assert.equal(host.firstChild.scrollTop, 400, 'Setter worked as expected.');
+      teardown();
+    });
+  })
 });
